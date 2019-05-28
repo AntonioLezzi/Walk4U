4aacb75afafe2bb9d19d744a07fe0d0172c9c864
AlessandroViolin91 <a.violin@itsrizzoli.it>
2019-05-28 17:03:13 +0200

    Aggiunta icona applicazione, modifiche alla scrmata sonno.

--- app/src/main/java/com/example/walk4you/Sonno.java
+++ app/src/main/java/com/example/walk4you/Sonno.java
@@ -2,12 +2,24 @@ package com.example.walk4you;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Sonno extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sonno);
+
+        Button buttonSalva=(Button) findViewById(R.id.buttonSalva);
+        buttonSalva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Ore di sonno salvate", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
