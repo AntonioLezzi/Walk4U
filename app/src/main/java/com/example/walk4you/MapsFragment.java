45d7d28eac4f299f6321013363b0b161b7451f75
AlessandroViolin91 <a.violin@itsrizzoli.it>
2019-05-30 11:58:51 +0200

    Aggiunta mappa (ancora non funzionante)

--- app/src/main/java/com/example/walk4you/MapsFragment.java
+++ app/src/main/java/com/example/walk4you/MapsFragment.java
@@ -4,9 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MapsFragment extends Fragment {
@@ -48,3 +57,5 @@ public class MapsFragment extends Fragment {
         mListener = null;
     }
 }
+
+
