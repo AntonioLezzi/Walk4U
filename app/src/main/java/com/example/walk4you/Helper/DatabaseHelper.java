8ea17c21ea009fd09cae010eaf8fc2412de1521b
AlessandroViolin91 <a.violin@itsrizzoli.it>
2019-05-30 16:50:30 +0200

    inserite le constati in un file java a parte e richiamate nell'helper

--- app/src/main/java/com/example/walk4you/Helper/DatabaseHelper.java
+++ app/src/main/java/com/example/walk4you/Helper/DatabaseHelper.java
@@ -4,34 +4,25 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.example.walk4you.util.Constants.*;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 1;
 
     private static final String DATABASE_NAME = "DbWalkForYou";
+    private static final int DATABASE_VERSION = 1;
 
-    public static final String TABELLA_UTENTE  = "Utente";
-
-    public static final String KEY_MAIL = "mail";
-    public static final String KEY_NOME = "nome";
-    public static final String KEY_COGNOME = "cognome";
-    public static final String KEY_PASSWORD = "password";
-    public static final String KEY_ALTEZZA = "altezza";
-    public static final String KEY_SESSO = "Sesso";
-    public static final String KEY_PUNTEGGIO = "Punteggio";
-    public static final String KEY_DATA_NASCITA ="data nascita";
-
+    private static DatabaseHelper databaseHelper;
 
 
     private static final String CREATE_TABELLA_UTENTE = "CREATE TABLE " +  TABELLA_UTENTE + "("
-            + KEY_MAIL + "INTEGER PRIMARY KEY,"
-            + KEY_NOME + "TEXT"
-            + KEY_COGNOME + "TEXT"
-            + KEY_PASSWORD + "TEXT"
-            + KEY_ALTEZZA +"NUMRO"
-            + KEY_SESSO + "TEXT"
-            + KEY_PUNTEGGIO + "NUMERO"
-            + KEY_DATA_NASCITA + "DATA" + ")";
+            + MAIL + "INTEGER PRIMARY KEY,"
+            + NOME + "TEXT"
+            + COGNOME + "TEXT"
+            + PASSWORD + "TEXT"
+            + ALTEZZA +"NUMRO"
+            + SESSO + "TEXT"
+            + DATA_NASCITA + "DATA" + ")";
 
     public DatabaseHelper (Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
---
+++ app/src/main/java/com/example/walk4you/util/Constants.java
@@ -0,0 +1,44 @@
+package com.example.walk4you.util;
+
+public class Constants {
+
+    //tabella utente
+    public static final String TABELLA_UTENTE  = "Utente";
+    public static final String MAIL = "mail";
+    public static final String NOME = "nome";
+    public static final String COGNOME = "cognome";
+    public static final String PASSWORD = "password";
+    public static final String ALTEZZA = "altezza";
+    public static final String SESSO = "Sesso";
+    public static final String DATA_NASCITA ="data nascita";                   // va inserito anche l'ID auto incrementante?
+
+
+    // tabella nutrienti
+
+    public static final String GRASSI_SATURI = "Grassi sauri";
+    public static final String FIBRE = "fibre";
+    public static final String PROTEINE = "proteine";
+    public static final String POTASSIO = "potassio";
+    public static final String FERRO = "ferro";
+    public static final String VITAMINA_A = "vitamia A";
+    public static final String VITAMINA_C = "vitamina C";
+    public static final String SODIO = "sodio";
+
+
+    // Tabella alimenti
+    public static final String INGREDIENTI = "ingredienti";
+    public static final String CODICE_A_BARRE = "codice a barre";
+
+    // Ristoranti
+    public static final String TIPOLOGIA_RISTORANTE = "tipologia ristorante";
+    public static final String DESCRIZIONE = "descrizione";
+    public static final String POSIZIONE_GPS = "posizione gps";       // non sono sicuro funzioni così
+
+    //premi
+    public static final String QR_CODE = "codice QR";
+    public static final String NOME_PREMIO = "Nome Premio";
+    public static final String PUNTEGGIO_PREMI = "punteggio premi";
+    public static final String LIVELLO_PREMI = "livello premi";
+
+}
+
