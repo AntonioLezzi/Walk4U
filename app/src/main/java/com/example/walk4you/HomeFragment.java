7c22aa36220022caabb70cf4a1d052bf1b0ce1d8
AlessandroViolin91 <a.violin@itsrizzoli.it>
2019-06-03 16:38:47 +0200

    Prove su mappe

--- app/src/main/java/com/example/walk4you/HomeFragment.java
+++ app/src/main/java/com/example/walk4you/HomeFragment.java
@@ -2,17 +2,11 @@ package com.example.walk4you;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDialog;
-import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 
--- app/src/main/java/com/example/walk4you/MapsFragment.java
+++ app/src/main/java/com/example/walk4you/MapsFragment.java
@@ -1,10 +1,10 @@
 package com.example.walk4you;
 
+import android.app.Activity;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
@@ -21,7 +22,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    MapView mapView;
+    GoogleMap map;
 
     private OnFragmentInteractionListener mListener;
 
@@ -42,17 +44,27 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_maps, container, false);
 
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mapView.getMapAsync(this);
+
         return v;
     }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
-
-            MapFragment mapFragment = (MapFragment) getActivity().getFragmentManager().findFragmentById(R.id.mapView);
-            mapFragment.getMapAsync(this);
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -67,13 +79,14 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
+        map.getUiSettings().setMyLocationButtonEnabled(false);
 
         LatLng firenze = new LatLng(43.776366, 11.247822);
-        mMap.addMarker(new MarkerOptions().position(firenze).title("Siamo a Firenze!"));
+        map.addMarker(new MarkerOptions().position(firenze).title("Siamo a Firenze!"));
         CameraPosition cameraPosition = new CameraPosition.Builder().target(firenze).zoom(15).build();
 
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
 
